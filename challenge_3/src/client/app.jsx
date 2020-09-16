e3f9736f5bcdcb953683dc5179d11029eb06e90a
Chris Bell <cb.camellia@gmail.com>
2020-09-16 15:24:09 -0700

    Add Identifier and Address components

--- challenge_3/src/client/app.jsx
+++ challenge_3/src/client/app.jsx
@@ -4,31 +4,135 @@ class App extends React.Component {
     super();
   }
 
+  // send data to the server
+  send(data) {
+    // data.target is an array, can access individual fields using name property
+  }
+
   render() {
     return (
-      <p>Hello</p>
+      <div>
+        <Checkout />
+        <Identifier next={this.send} />
+        <Address next={this.send} />
+      </div>
     );
   }
 };
 
 // checkout page
-let Checkout = ({nextCallback}) => {
-
+let Checkout = ({ next }) => {
+  return (
+    <button id="checkout" onClick={next}>Checkout!</button>
+  )
 }
 
 // user info form
-let Identifier = ({nextCallback}) => {
+let Identifier = ({ next }) => {
 
+  return (
+    <form onSubmit={next}>
+      <fieldset className="account-info">
+        <legend>Account Details</legend>
+
+        <label htmlFor="first-name">First Name</label>
+        <input type="text" name="first-name" required></input>
+
+        <label htmlFor="last-name">Last Name</label>
+        <input type="text" name="last-name" required></input>
+
+        <label htmlFor="email-address">Email</label>
+        <input type="email" name="email-address" required></input>
+
+        <label htmlFor="password">Password</label>
+        <input type="password" required></input>
+        <input type="btn" type="submit" name="submit" value="Next"></input>
+      </fieldset>
+    </form>
+  );
 }
 
 // address form
-let Address = ({nextCallback}) => {
+let Address = ({ next }) => {
 
+  return (
+    <form onSubmit={next}>
+      <fieldset className="address">
+        <legend>Address</legend>
+
+        <label htmlFor="line-1">Line 1</label>
+        <input type="text" name="line-1" required></input>
+
+        <label htmlFor="line-2">Line 2</label>
+        <input type="text" name="line-2"></input>
+
+        <label htmlFor="state">State</label>
+        <select required>
+          <option value="AL">Alabama</option>
+          <option value="AK">Alaska</option>
+          <option value="AZ">Arizona</option>
+          <option value="AR">Arkansas</option>
+          <option value="CA">California</option>
+          <option value="CO">Colorado</option>
+          <option value="CT">Connecticut</option>
+          <option value="DE">Delaware</option>
+          <option value="DC">District Of Columbia</option>
+          <option value="FL">Florida</option>
+          <option value="GA">Georgia</option>
+          <option value="HI">Hawaii</option>
+          <option value="ID">Idaho</option>
+          <option value="IL">Illinois</option>
+          <option value="IN">Indiana</option>
+          <option value="IA">Iowa</option>
+          <option value="KS">Kansas</option>
+          <option value="KY">Kentucky</option>
+          <option value="LA">Louisiana</option>
+          <option value="ME">Maine</option>
+          <option value="MD">Maryland</option>
+          <option value="MA">Massachusetts</option>
+          <option value="MI">Michigan</option>
+          <option value="MN">Minnesota</option>
+          <option value="MS">Mississippi</option>
+          <option value="MO">Missouri</option>
+          <option value="MT">Montana</option>
+          <option value="NE">Nebraska</option>
+          <option value="NV">Nevada</option>
+          <option value="NH">New Hampshire</option>
+          <option value="NJ">New Jersey</option>
+          <option value="NM">New Mexico</option>
+          <option value="NY">New York</option>
+          <option value="NC">North Carolina</option>
+          <option value="ND">North Dakota</option>
+          <option value="OH">Ohio</option>
+          <option value="OK">Oklahoma</option>
+          <option value="OR">Oregon</option>
+          <option value="PA">Pennsylvania</option>
+          <option value="RI">Rhode Island</option>
+          <option value="SC">South Carolina</option>
+          <option value="SD">South Dakota</option>
+          <option value="TN">Tennessee</option>
+          <option value="TX">Texas</option>
+          <option value="UT">Utah</option>
+          <option value="VT">Vermont</option>
+          <option value="VA">Virginia</option>
+          <option value="WA">Washington</option>
+          <option value="WV">West Virginia</option>
+          <option value="WI">Wisconsin</option>
+          <option value="WY">Wyoming</option>
+        </select>
+
+        <label htmlFor="zip">Zip Code</label>
+        <input type="text" name="zip" required></input>
+
+        <input type="btn" type="submit" name="submit" value="Next"></input>
+      </fieldset>
+    </form>
+  );
 }
 
 // payment and submit form
-let Payment = ({nextCallback}) => {
-
+let Payment = ({ next }) => {
+  return null;
 }
 
 // render page onload
